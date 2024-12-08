8acb280d686f741f730b0a4dea3eee381bbc05af
HanaOmar1 <Hanaomar20043@gmail.com>
2024-12-08 23:20:31 +0200

    dev b

--- main.cpp
+++ main.cpp
@@ -7,16 +7,12 @@ int sum(int a, int b) {
 }
 
 int main() {
-    int num1, num2;
+    
 
-    cout << "Enter the first number: ";
-    cin >> num1;
+    cout << "DEV BRANCH";
+    
 
-    cout << "Enter the second number: ";
-    cin >> num2;
-
-    // Call the sum function and display the result
-    cout << "The sum of " << num1 << " and " << num2 << " is: " << sum(num1, num2) << endl;
+    
 
     return 0;
 }
